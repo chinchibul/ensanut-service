735b0d004bb1fecd4b3e82a454ed37a0c075b553
Jose Luis Gordillo Ruiz <jlgr@c3.unam.mx>
2025-06-23 17:13:20 -0600

    servicio multi-"modo".
    ofrece una fuente de datos con variables tanto en ensambles espaciales
    como individuales.
    
    las variables vienen ahora en archivos separados, pero el servicio las
    despacha igual, solo importa el campo 'available_grids'.
    
    en este punto solo funciona completamente el endpoint /variables
    
    los otros dos funcionan solo con las variables individuales.

--- service.py
+++ service.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Response
+from flask import Response, request
 import pandas as pd
 import json
 import ast
@@ -11,12 +11,21 @@ import ast
 variables = pd.read_csv("variables.csv")
 variables["id"] = variables.index
 variables["level_size"] = 1
-variables["filter_fields"] = [ [] for i in range(0, len(variables)) ] 
-mallas = [ ["ensanut"] for i in range(0, len(variables)) ]
-variables["available_grids"] = mallas
+variables["filter_fields"] = [ [] for i in range(0, len(variables)) ]
+variables["available_grids"] = variables["available_grids"].apply(ast.literal_eval)
 variables.rename(columns={"var":"name"}, inplace=True)
+variables_mun = pd.read_csv("variables_muns.csv")
+variables_mun["id"] = variables_mun.index + len(variables)
+variables_mun["level_size"] = 1
+variables_mun["filter_fields"] = [ [] for i in range(0, len(variables_mun)) ]
+variables_mun["available_grids"] = variables_mun["available_grids"].apply(ast.literal_eval)
+variables_mun.rename(columns={"var":"name"}, inplace=True)
 
+lista_var = variables[["id", "name", "level_size", "filter_fields", "available_grids"]].to_dict(orient='records')
+lista_var_muns = variables_mun[["id", "name", "level_size", "filter_fields", "available_grids"]].to_dict(orient='records')
 
+lista_var.extend(lista_var_muns)
+lista_total = json.dumps(lista_var)
 
 app = Flask(__name__)
 
@@ -25,21 +34,34 @@ def hello_world():
     return "<p>Hello!</p>"
 
 
-@app.route("/variables")
+@app.route("/variables/")
 def me_api():
-    return Response(response=variables[["id", "name", "level_size", "filter_fields", "available_grids"]].to_json(orient="records"), status=200, mimetype="application/json")
+    respuesta = Response(response=lista_total, status=200, mimetype="application/json")
+    return respuesta
+
+@app.route("/variables/<id>")
+def single_var(id):
+    q = request.args.get('q', '*')
+    datos = []
+    level_id = 0
+    print(variables.columns)
+    for columna in ["name", "rango"]:
+        datos.append({"level_id": level_id, "id":id,  columna:variables.loc[variables["id"] == int(id), columna].values[0]})
+        level_id = level_id + 1
+    respuesta = Response(response=json.dumps(datos), status=200, mimetype="application/json")
+    return respuesta
 
 
 
 @app.route('/get-data/<id>')
 def get_data_id(id):
     """Obtiene datos específicos de una covariable por ID y filtros opcionales."""
-    print(type(id), id)
-
+    levels_id = request.args.get('levels_id', "[0]")
+    levels_id = ast.literal_eval(levels_id)
+    list_of_levels = variables.columns[2:4]
+    levels_response = [variables.loc[variables["id"] == int(id), list_of_levels[i]].values[0] for i in levels_id]
     lista = ast.literal_eval(variables.loc[int(id)]["valores"])
-    print(len(lista))
-                             
-    respuesta = [{"id": id, "grid_id":"ensanut", "level_id":"1","n":len(lista), "cells":lista }]
+    respuesta = [{"id": id, "grid_id":"ensanut", "level_id":levels_response,"n":len(lista), "cells":lista }]
     return Response(response=json.dumps(respuesta), mimetype="application/json")
 
 
